5eb73646041567ec84e162cbe58310a114048d8a
Katrina Hernandez <abqkatrina@gmail.com>
2020-01-17 16:30:53 -0700

    finally!

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -19,7 +19,39 @@
 
 
 
+function CreateCard(item){
 
+    //constructing elements
+    const card = document.createElement('div');
+
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const photoBox = document.createElement('div');
+    const img = document.createElement('img');
+    const byAuthor = document.createElement('span');
+
+    // assigning classes
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    photoBox.classList.add('img-container');
+
+    //oranizing elements
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(photoBox);
+    author.appendChild(byAuthor);
+    photoBox.appendChild(img);
+
+    //adding content
+    headline.textContent = (item.headline);
+    img.src = (item.authorPhoto);
+    byAuthor.textContent = (item.authorName);
+
+
+    return card;
+    
+}
 
 
 
@@ -36,69 +68,18 @@ axios.get('https://lambda-times-backend.herokuapp.com/articles')
     //returns data object
     //console.log(response.data)
     //returns article object
-    console.log(response.data.articles);
-    Object.values(response.data.articles.javascript).forEach(value => { 
-    //console.log(value),
-    wall.append(CreateCard(value))
-    })
+    //console.log(response.data.articles);
 
-    Object.values(response.data.articles.boostrap).forEach(value => { 
-    //console.log(value),
-    wall.append(CreateCard(value))
-    })
-
-    Object.values(response.data.articles.technology).forEach(value => { 
-    //console.log(value),
-    wall.append(CreateCard(value))
-    })
-  
-    Object.values(response.data.articles.jQuery).forEach(value => { 
-    //console.log(value),
-    wall.append(CreateCard(value))
-    })
+    Object.keys(response.data.articles).forEach((key, index) => {
+        //console.log(key, response.data.articles[key]);
+        
+        response.data.articles[key].forEach(entry => {
+            wall.append(CreateCard(entry));
+        });
  
-    // Object.values(response.data.articles.node\.js).forEach(value => { 
-    // //console.log(value),
-    // wall.append(CreateCard(value))
-    // })
+    })
 })
-
 
 .catch((error) => {
-    console.log('dangnabbit', error);
+    console.log('Dangnabbit!', error);
 })
-
-
-function CreateCard(article){
-
-    //constructing elements
-    const card = document.createElement('div');
-
-    const headline = document.createElement('div');
-    const author = document.createElement('div');
-    const photoBox = document.createElement('div');
-    const img = document.createElement('img');
-    const byAuthor = document.createElement('span');
-
-    // assigning classes
-    card.classList.add('card');
-    headline.classList.add('headline');
-    author.classList.add('author');
-    photoBox.classList.add('img-container');
-    img.src = (article.authorPhoto);
-
-    //oranizing elements
-    card.appendChild(headline);
-    card.appendChild(author);
-    author.appendChild(photoBox);
-    author.appendChild(byAuthor);
-    photoBox.appendChild(img);
-
-    //adding content
-    headline.textContent = article.headline;
-    byAuthor.textContent = article.authorName;
-
-
-    return card;
-    
-}
